a30499d049f1cccd57395095309a314ee319f6f8
hannahashe <hannahashe13@gmail.com>
2026-02-02 09:56:08 +0000

    Update index view to handle POST requests and return appropriate responses

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index (request):
-    return HttpResponse("Hello, World!")
+def index(request):
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
